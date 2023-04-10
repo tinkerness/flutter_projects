e562a5fd292e1db00e96b69a1e9087d520496645
Anitta Siby <anittasiby002@gmail.com>
2023-04-10 20:14:37 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print('hello');
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lime,
+      ),
+    )
+  );
+}
